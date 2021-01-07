a133171e33f52ca1d1adc1720381266e661c18fb
daeun <sung980524@naver.com>
2021-01-07 12:56:57 +0900

    Initial commit

---
+++ src/test/java/com/spring/springcaffemore/SpringCaffemoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springcaffemore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCaffemoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
